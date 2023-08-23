d19671bd762214f494edb5cfd2ac35ca44697c7e
Thuan Nguyen <cs2be@Thuans-MacBook-Pro.local>
2023-08-23 15:58:21 -0700

    update parser

--- chatgpt_demo/chatgpt_demo/models.py
+++ chatgpt_demo/chatgpt_demo/models.py
@@ -4,6 +4,7 @@ class Listing(models.Model):
     name = models.CharField(max_length=1000)
     description = models.CharField(max_length=1000)
     url = models.CharField(max_length=5000)
+    full_description = models.TextField(default='')
 
     def __str__(self):
         return self.name + ', url: ' + self.url
--- chatgpt_demo/chatgpt_demo/views.py
+++ chatgpt_demo/chatgpt_demo/views.py
@@ -6,6 +6,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+from requests_html import HTMLSession
+
+import urllib.request
 
 @api_view(['GET', 'POST'])
 def listing_list(request):
@@ -18,22 +21,64 @@ def listing_list(request):
      
     if request.method == 'POST':
         url = request.data['url']
-        _parse_data(url)
-        return Response(request.data['url'], status = status.HTTP_201_CREATED)
-        # serializer = ListingSerializer(data = request.data) 
-        # if serializer.is_valid():
-        #     listing = serializer.data
-        #     # serializer.save()
-        #     print('GOT LISTING: ' + list.url)
-        #     return Response(serializer.data, status = status.HTTP_201_CREATED)
+        serializer = _parse_data(url)
+        if serializer.is_valid():
+            # listing = serializer.data
+            # serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
 
 def _parse_data(url):
     print('Parsing URL: ' + url)
-    webpage = urlopen(url).read()
-    soup = BeautifulSoup(webpage, "lxml")
+    # webpage = urlopen(url).read()
 
-    meta_tags = soup.find("meta")
+    # s = HTMLSession()
+    # response = s.get(url)
+    # response.html.render()
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            html = response.read().decode('utf-8')#use whatever encoding as per the webpage
+    except urllib.request.HTTPError as e:
+        if e.code==404:
+            print(f"{url} is not found")
+        elif e.code==503:
+            print(f'{url} base webservices are not available')
+            ## can add authentication here 
+        else:
+            print('http error',e)
+
+    # print(html)
+    f = open("output.html", "w")
+    f.write(html)
+    f.close()
+
+    soup = BeautifulSoup(html, "html.parser")
+
+    meta_tags = soup.find_all("meta")
+
+    parsed_name = ''
+    parsed_description = ''
+    parsed_url = ''
+    parsed_full_description = ''
+    
     for tag in meta_tags:
-        print('Tag: ' + tag)
-    # serializer = ListingSerializer(data = {'name' = soup.}) 
+        # print(tag)
+        if tag.get('property', None) == 'og:title':
+            parsed_name = tag.get('content', None)
+        elif tag.get('property', None) == 'og:description':
+            parsed_description = tag.get('content', None)
+        elif tag.get('property', None) == 'og:url':
+            parsed_url = tag.get('content', None)
+
+# for el in soup.select("[gender='male']"):
+
+    full_description = soup.select("[data-section-id='DESCRIPTION_DEFAULT']")
+
+    print("NAME: " + parsed_name)
+    print("DESCRIPTION: " + parsed_description)
+    print("URL: " + parsed_url)
+    print(full_description)
+    return ListingSerializer(data = {'name': parsed_name, 'description': parsed_description, 'url': parsed_url})
+
+
 
